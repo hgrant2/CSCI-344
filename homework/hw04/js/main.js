d48c93cbf085dfd903c953fb0c73c968a3816bd6
Haley <hgrant2@unca.edu>
2023-03-12 14:28:23 -0400

    stories and user profile upload from initPage

--- homework/hw04/js/main.js
+++ homework/hw04/js/main.js
@@ -1,6 +1,10 @@
 import {getAccessToken} from './utilities.js';
 const rootURL = 'https://photo-app-secured.herokuapp.com';
 
+
+//TODO
+//finish post to html
+
 const showStories = async (token) => {
     const endpoint = `${rootURL}/api/stories`;
     const response = await fetch(endpoint, {
@@ -11,10 +15,52 @@ const showStories = async (token) => {
     })
     const data = await response.json();
     console.log('Stories:', data);
+    const htmlChunk = data.map(storyToHtml).join('');
+    document.querySelector('.stories').innerHTML = htmlChunk;
 }
 
+const storyToHtml = story => {
+    return `<div>
+        <img src="${story.user.thumb_url}" class="pic" />
+        <p>${story.user.username}</p>
+    </div>
+    `
+}
+
+const showRightPanel = async (token) => {
+    const endpoint = `${rootURL}/api/profile`;
+    const response = await fetch(endpoint, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+    const data = await response.json();
+    console.log('Right Panel:', data);
+   const htmlChunk = `<img src="${data.thumb_url}" class="pic" />
+   <h2>${data.username}</h2>`;
+    document.querySelector('aside header').innerHTML = htmlChunk;
+}
+
+
 const showPosts = async (token) => {
-    console.log('code to show posts');
+    console.log('now testing code to show post');
+    const endpoint = `${rootURL}/api/posts`;
+    const response = await fetch(endpoint, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+        }
+    })
+    const data = await response.json();
+    console.log('Posts:', data);
+}
+
+const postToHtml = post => {
+    return `<section>
+    
+    
+    </section>`;
 }
 
 
@@ -25,6 +71,7 @@ const initPage = async () => {
     // then use the access token provided to access data on the user's behalf
     showStories(token);
     showPosts(token);
+    showRightPanel(token);
 }
 
 initPage();
