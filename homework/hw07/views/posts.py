7667141761a3f5b861633554671ee5cd24e81864
Haley <hgrant2@unca.edu>
2023-04-07 15:20:02 -0400

    Added stuff from tutorial in class friday

--- homework/hw07/views/posts.py
+++ homework/hw07/views/posts.py
@@ -1,6 +1,6 @@
 from flask import Response, request
 from flask_restful import Resource
-from models import Post, db
+from models import Post, Following, db
 from views import get_authorized_user_ids
 
 import json
@@ -14,21 +14,51 @@ class PostListEndpoint(Resource):
         self.current_user = current_user
 
     def get(self):
-        # get posts created by one of these users:
-        posts = Post.query.limit(5).all()
-        return Response(json.dumps([post.to_dict() for post in posts]), mimetype="application/json", status=200)
+        print("TESTING IDK WTF THIS IS I GOT SPA BRAIN", request.args.get('limit'))
+        lim = 20
+        try:
+            if request.args.get('limit'):
+                lim = int(request.args.get('limit'))
+        except:
+            return Response(
+                json.dumps({'error': "Bad data. You can't put letters in as a limit."}), status=400
+            ) 
+        if lim > 20:
+            #lim = 20
+            return Response(
+                json.dumps({'error': "Bad data fuck off ya hacker. Don't go over 20."}), status=400
+            )
+        
+        
 
+        # lim request.args.get('limit') or 20
+        #like wtf is this python is so simple it is stupid
+
+        # get posts created by one of these users:
+        following = Following.query.filter_by(user_id=self.current_user.id).all()
+       
+        #building a list of our friend usernames       
+        friend_ids = []
+        for record in following:
+            friend_ids.append(record.following_id)
+        friend_ids.append(self.current_user.id)
+        print(friend_ids)
+        
+
+        posts = Post.query.filter(Post.user_id.in_(friend_ids)).limit(lim)
+        return Response(json.dumps([post.to_dict() for post in posts]), mimetype="application/json", status=200)
+       
     def post(self):
         # create a new post based on the data posted in the body 
         body = request.get_json()
         print(body)  
         return Response(json.dumps({}), mimetype="application/json", status=201)
-        
+
 class PostDetailEndpoint(Resource):
 
     def __init__(self, current_user):
         self.current_user = current_user
-        
+
 
     def patch(self, id):
         # update post based on the data posted in the body 
