557ad813c0f5c22e40288e2af6ee6299eb4f838e
Haley <hgrant2@unca.edu>
2023-04-17 14:48:51 -0400

    WIP on bookmark post

--- homework/hw07/views/bookmarks.py
+++ homework/hw07/views/bookmarks.py
@@ -19,7 +19,44 @@ class BookmarksListEndpoint(Resource):
         # create a new "bookmark" based on the data posted in the body 
         body = request.get_json()
         print(body)
-        return Response(json.dumps({}), mimetype="application/json", status=201)
+
+        #check if post id is an int or not
+        try:
+            num = int(body.post_id)
+        except:
+            return Response(json.dumps({'error': 'Incorrect format, The post_id is not an int'}),mimetype="application/json", status=400)
+
+        
+        new_bookmark = Bookmark(
+            user_id=self.current_user.id,
+            post_id=body.get('post_id')
+        )
+
+        bookmarks = Bookmark.query.filter_by(user_id =self.current_user.id).all()
+        # print("bookmarks without to_dict()", bookmarks)
+        # print("Now printing the bookmarks: ", [bookmark.to_dict() for bookmark in bookmarks])
+
+        #idea
+        #make a list of all of the post ids that are currently bookmarked by our current user
+        #if bookmark exist send error saying it already exist
+        #else make a new bookmark and add it to db
+
+        list_of_post_ids_in_bookmarks = []
+        for bookmark in bookmarks:
+            list_of_post_ids_in_bookmarks.append(bookmark.post_id)
+        
+        print("printing list of post ids from currently bookmarked post by current user: ", list_of_post_ids_in_bookmarks)
+
+        if(new_bookmark.post_id in list_of_post_ids_in_bookmarks):
+            print("bookmark already exist")
+            return Response(json.dumps({'error': 'This post is already bookmarked'}),mimetype="application/json", status=400)
+        
+        db.session.add(new_bookmark)
+        db.session.commit()
+        # print("printing new bookmark before to_dict(): ",new_bookmark)
+        # print("printing new bookmark after to_dict(): ", new_bookmark.to_dict())
+
+        return Response(json.dumps(new_bookmark.to_dict()), mimetype="application/json", status=201)
 
 class BookmarkDetailEndpoint(Resource):
 
--- homework/hw07/views/posts.py
+++ homework/hw07/views/posts.py
@@ -2,6 +2,7 @@ from flask import Response, request
 from flask_restful import Resource
 from models import Post, Following, db
 from views import get_authorized_user_ids
+import access_utils
 
 import json
 
@@ -55,33 +56,24 @@ class PostListEndpoint(Resource):
         return Response(json.dumps([post.to_dict() for post in posts]), mimetype="application/json", status=200)
        
     def post(self):
-        # create a new post based on the data posted in the body
-        # 
-        # request.get_jason() is holding the data the user
-        # just sent over  
+       # request.get_json() is holding the data that the user
+        # just sent over the network. Stored as a python dictionary
         body = request.get_json()
-        # 1. Create:
-        #converting the data the the user sent over http to a SQLAlchemy object
-
         if not body.get('image_url'):
-            return(Response(json.dumps()))
+            return Response(json.dumps({'error': 'image_url required'}), status=400)
 
+        # convert the data that the user sent over HTTP to a SQLAlchemy object
         new_post = Post(
             image_url=body.get('image_url'),
             user_id=self.current_user.id, # must be a valid user_id or will throw an error
             caption=body.get('caption'),
             alt_text=body.get('alt_text')
         )
-
         # save it to the database
         db.session.add(new_post)    # issues the insert statement
-        db.session.commit()         # commits the change to the database 
+        db.session.commit()   
 
-
-
-        print(body)  
-
-        # send the new data ovject to the user
+        # send the new data object to the user
         return Response(json.dumps(new_post.to_dict()), mimetype="application/json", status=201)
 
 class PostDetailEndpoint(Resource):
@@ -89,60 +81,70 @@ class PostDetailEndpoint(Resource):
     def __init__(self, current_user):
         self.current_user = current_user
 
-
+    @access_utils.can_modify_or_404
     def patch(self, id):
-        # if I want to update a post,
-        # 1. go get current post and check if they are authorized
-        # 2. update only the items that the user changed
-        # 3. save it again
-        # 4. return final repersentation to user
+        '''
+        If I want to update a post, what do I do?
+        1. Go get the current post. And check if they're authorized.
+        2. Update only the items that the user changed
+        3. Save to the DB
+        4. Return the final representation to the user
+        '''
         # update post based on the data posted in the body 
         body = request.get_json()
-        print(body)       
 
         post = Post.query.get(id)
+
+        # only update the data fields that the user wants to change:
         if body.get('image_url'):
-            post.image_url = body.get('image_url')
+            post.image_url = body.get('image_url') 
         if body.get('caption'):
-            post.caption = body.get('caption')
+            post.caption = body.get('caption')  
         if body.get('alt_text'):
-            post.alt_text = body.get('alt_text')   
+            post.alt_text = body.get('alt_text')  
 
-
-        # save database
+        # save back to database:
         db.session.commit() 
+
         return Response(json.dumps(post.to_dict()), mimetype="application/json", status=200)
-
-
+    
+    @access_utils.can_modify_or_404
     def delete(self, id):
-        # delete post where "id"=id
-        return Response(json.dumps({}), mimetype="application/json", status=200)
+        Post.query.filter_by(id=id).delete()
+        db.session.commit()
+        return Response(json.dumps(None), mimetype="application/json", status=200)
+
+    # def get(self, id):
+    #     # get the post based on the id
+    #     print("what is the id")
+    #     print(id)
+
+    #     # post = Post.query.get(id)
+    #     # print("PRINTING THE DAMN POST ", post)
+
+    #      # get posts created by one of these users:
+    #     following = Following.query.filter_by(user_id=self.current_user.id).all()
+       
+    #     #building a list of our friend usernames       
+    #      # get the post based on the id
+    #     # yourself and your friends
+    #     post = Post.query.get(id)
+    #     me_and_my_friend_ids = get_list_of_user_ids_in_my_network(self.current_user.id)
+    #     if  post is None or post.user_id not in me_and_my_friend_ids:
+    #         error_message = {
+    #             'error': 'post {0} does not exist.'.format(id)
+    #         }
+    #         return Response(json.dumps(error_message), mimetype="application/json", status=404)
+    #     else:
+    #         return Response(json.dumps(post.to_dict()), mimetype="application/json", status=200)
 
 
+    @access_utils.can_view_or_404
     def get(self, id):
         # get the post based on the id
-        print("what is the id")
-        print(id)
-
-        # post = Post.query.get(id)
-        # print("PRINTING THE DAMN POST ", post)
-
-         # get posts created by one of these users:
-        following = Following.query.filter_by(user_id=self.current_user.id).all()
-       
-        #building a list of our friend usernames       
-         # get the post based on the id
         # yourself and your friends
         post = Post.query.get(id)
-        me_and_my_friend_ids = get_list_of_user_ids_in_my_network(self.current_user.id)
-        if  post is None or post.user_id not in me_and_my_friend_ids:
-            error_message = {
-                'error': 'post {0} does not exist.'.format(id)
-            }
-            return Response(json.dumps(error_message), mimetype="application/json", status=404)
-        else:
-            return Response(json.dumps(post.to_dict()), mimetype="application/json", status=200)
-
+        return Response(json.dumps(post.to_dict()), mimetype="application/json", status=200)
     #check if post is there, then check if post was made by the user we are
     # else 404 because they do not have access???? please be fucking right
 
